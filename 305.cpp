b791197d76e04e9925b6c06532dde5ce10a77955
bertrand <714356388@qq.com>
2015-12-02 04:11:44 +0800

    add annotation

--- 305.cpp
+++ 305.cpp
@@ -15,27 +15,29 @@ class Solution {
 		vector<vector<int>> board(m, vector<int>(n, -1));
 		vector<int> res;
 		for(auto& item : positions){
-			board[item.first][item.second] = item.first * board[0].size() + item.second;
-			num_islands = num_islands + 1 - merged(board, item.first, item.second);
+			num_islands = num_islands + 1 - merge(board, item.first, item.second);
 			res.push_back(num_islands);
 		}
 		return res;
 	}
-	int merged(vector<vector<int>>& board, int row, int col){
-		set<int> neighber_set;
+	// root tree containing neighbour node to self
+	// not as efficienty as rooting lower tree to higher tree, modified for simplicity
+	int merge(vector<vector<int>>& board, int row, int col){
+	        board[row][col] = row * board[0].size() + col;
+		set<int> neighbour_set;
 		for(auto& item : directions){
 			int check_row = row + item.first;
 			int check_col = col + item.second;
 			if (check_row >= 0 && check_row < board.size() && check_col >= 0 && check_col < board[0].size() && board[check_row][check_col] != -1 && board[check_row][check_col] != board[row][col]){
 				path_compression(board, check_row, check_col);
 				if (board[check_row][check_col] != board[row][col]){
-					neighber_set.insert(board[check_row][check_col]);
+					neighbour_set.insert(board[check_row][check_col]); // there may be neighbours share root;
 					int neigh_father = board[check_row][check_col] ;
-					board[neigh_father / board[0].size()][neigh_father % board[0].size()] = board[row][col];
+					board[neigh_father / board[0].size()][neigh_father % board[0].size()] = board[row][col]; // neighbour -> neighbour'root-> self
 				}
 			}
 		}
-		return neighber_set.size();
+		return neighbour_set.size(); // num of unique trees containing neighbour node
 	}
 	void path_compression(vector<vector<int>>& board, int row, int col){
 		int father = board[row][col];
